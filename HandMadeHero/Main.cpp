74b820d954b23c5de0caed3f2493e892cddd9102
Shivansh Coder <shivanshcoder@outlook.com>
2018-09-28 01:51:13 +0530

    Half Day009 completed and still that hiccup in sound need to fix that

--- HandMadeHero/Main.cpp
+++ HandMadeHero/Main.cpp
@@ -2,12 +2,21 @@
 #include<stdint.h>
 #include<Xinput.h>
 #include<dsound.h>
+#include<math.h>
 
+float const Pi = 3.14159265359;
 
 #define global_variable static
 #define local_persist static
 
 global_variable bool Running;
+
+#pragma region __SCREENBUFFER
+
+struct Window_Dimension {
+	int Width;
+	int Height;
+};
 
 struct Offscreen_Buffer {
 	BITMAPINFO Info;
@@ -18,20 +27,135 @@ struct Offscreen_Buffer {
 	int Pitch;
 };
 
-static Offscreen_Buffer BackBuffer;
-global_variable LPDIRECTSOUNDBUFFER SecondaryBuffer;
 
-struct WindowDimension {
-	int Width;
-	int Height;
-};
+static Offscreen_Buffer BackBuffer;
+
+Window_Dimension GetWinDimension(HWND Window) {
+	Window_Dimension Result;
+	RECT ClientRect;
+	GetClientRect(Window, &ClientRect);
+	Result.Width = ClientRect.right - ClientRect.left;
+	Result.Height = ClientRect.bottom - ClientRect.top;
+
+	return Result;
+}
+
+static void RenderGradient(Offscreen_Buffer *Buffer, int xOffset, int yOffset) {
+	Buffer->Pitch = Buffer->Width * Buffer->BytesPerPixel;
+
+	uint8_t *Row = (uint8_t *)Buffer->Memory;
+
+	for (int y = 0; y < Buffer->Height; ++y) {
+
+		/*
+		incase you want 64bit filling
+		uint64_t *Pixel = (uint64_t *)Row;
+		*/
+		uint32_t *Pixel = (uint32_t *)Row;
+
+		for (int x = 0; x < Buffer->Width; x++) {
+			/*
+				Memory
+				BB GG RR xx
+
+				When loaded up in memory
+
+				Register
+				xx RR GG BB
+			*/
+			uint8_t Blue = x + xOffset;
+			uint8_t Green = y + yOffset;
+			uint8_t Red = 255;//(x + y + xOffset+yOffset) % 255;
+
+			/*
+
+			incase you want 64bit filling
+			x++;
+
+			uint8_t Blue2 = x + *xOffset;
+			uint8_t Green2 = y + *yOffset;
+			uint8_t Red2 = 0;
+
+
+				BB GG RR xx BB GG RR xx
+				xx RR GG BB xx RR GG BB
+
+
+			uint64_t temp = ((Red2 << 16) | (Green2 << 8) | Blue2);
+			temp = temp << 32;
+			temp = temp | (Red << 16) | (Green << 8) | (Blue);
+
+			*Pixel = temp;
+
+			*/
+
+			*Pixel = (Red << 16) | (Green << 8) | Blue;
+			Pixel++;
+		}
+		Row += Buffer->Pitch;
+	}
+}
+
+
+/*
+	Resizes and Initilizes the Information for our Back Bitmap Buffer
+*/
+static void WinResizeDIBSection(Offscreen_Buffer *Buffer, int Width, int Height) {
+
+
+	//Free the Memory if already allocated
+	if (Buffer->Memory)
+		VirtualFree(Buffer->Memory, 0, MEM_RELEASE);
+
+
+	Buffer->Height = Height;
+	Buffer->Width = Width;
+
+	Buffer->Info.bmiHeader.biSize = sizeof(Buffer->Info.bmiHeader);
+
+	Buffer->Info.bmiHeader.biWidth = Buffer->Width;
+	//Negative because we want bitmap to be Top down
+	Buffer->Info.bmiHeader.biHeight = -Buffer->Height;
+
+	Buffer->Info.bmiHeader.biPlanes = 1;
+	Buffer->Info.bmiHeader.biBitCount = 32;
+	Buffer->Info.bmiHeader.biCompression = BI_RGB;
+
+	//Number of Pixels we need 
+	//Bytes per pixel Represent
+	// BB GG RR xx 4 bytes
+	int BitMapMemorySize = (Buffer->Width* Buffer->Height)*Buffer->BytesPerPixel;
+
+	Buffer->Memory = VirtualAlloc(0, BitMapMemorySize, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+
+	//RenderGradient (Buffer, 100, 100);
+
+}
+
+void UpdateWin(Offscreen_Buffer *Buffer, HDC DeviceContext, int WindowWidth, int WindowHeight, int X, int Y/*, int Width, int Height*/) {
+
+	StretchDIBits(DeviceContext,
+
+		//Bitmap gets stretched according to it
+		0, 0, WindowWidth, WindowHeight,
+
+		//BufferWidth remains same
+		0, 0, Buffer->Width, Buffer->Height,
+
+		Buffer->Memory, &Buffer->Info, DIB_RGB_COLORS, SRCCOPY);
+}
+
+
+#pragma endregion
+
+#pragma region __XINPUT
 
 //GetState
 #define X_INPUT_GET_STATE(name) DWORD WINAPI name(DWORD dwUserIndex, XINPUT_STATE *pState)
 
-typedef X_INPUT_GET_STATE (x_input_get_state);
+typedef X_INPUT_GET_STATE(x_input_get_state);
 
-X_INPUT_GET_STATE (XInputGetStateStub) {
+X_INPUT_GET_STATE(XInputGetStateStub) {
 	return ERROR_DEVICE_NOT_CONNECTED;
 }
 
@@ -42,32 +166,28 @@ global_variable x_input_get_state *XInputGetState_ = XInputGetStateStub;
 
 //SetState
 #define X_INPUT_SET_STATE(name) DWORD WINAPI name(DWORD dwUserIndex, XINPUT_VIBRATION *pVibration)
-typedef X_INPUT_SET_STATE (x_input_set_state);
+typedef X_INPUT_SET_STATE(x_input_set_state);
 
-X_INPUT_SET_STATE (XInputSetStateStub) {
+X_INPUT_SET_STATE(XInputSetStateStub) {
 	return ERROR_DEVICE_NOT_CONNECTED;
 }
 global_variable x_input_set_state *XInputSetState_ = XInputSetStateStub;
 
 #define XInputSetState XInputSetState_
 
-#define DIRECT_SOUND_CREATE(name) HRESULT WINAPI name(LPGUID lpGuid, LPDIRECTSOUND* ppDS, LPUNKNOWN  pUnkOuter)
-
-typedef DIRECT_SOUND_CREATE(direct_sound_create);
-
-void Win32LoadXInput (void) {
+void Win32LoadXInput(void) {
 
 	//TODO Check which is needed for Windows 10 or Windows 7!
-	HMODULE XInputLibrary = LoadLibraryA ("xinput1_3.dll");
+	HMODULE XInputLibrary = LoadLibraryA("xinput1_3.dll");
 
 	if (!XInputLibrary)
 		XInputLibrary = LoadLibraryA("xinput1_4.dll");
 
 	if (XInputLibrary) {
-		XInputGetState = (x_input_get_state *)GetProcAddress (XInputLibrary, "XInputGetState");
+		XInputGetState = (x_input_get_state *)GetProcAddress(XInputLibrary, "XInputGetState");
 		if (!XInputGetState) { XInputGetState = XInputGetStateStub; }
 
-		XInputSetState = (x_input_set_state *)GetProcAddress (XInputLibrary, "XInputSetState");
+		XInputSetState = (x_input_set_state *)GetProcAddress(XInputLibrary, "XInputSetState");
 		if (!XInputSetState) { XInputSetState = XInputSetStateStub; }
 
 	}
@@ -76,6 +196,42 @@ void Win32LoadXInput (void) {
 	}
 }
 
+#pragma endregion
+
+#pragma region __DIRECTSOUND
+
+global_variable LPDIRECTSOUNDBUFFER SecondaryBuffer;
+
+struct Sound_Output {
+	int SamplesPerSecond;
+	uint32_t RunningSampleIndex;
+	int ToneHz;
+	int SquareWavePeriod;
+	int BytesPerSample;
+	int SecondaryBufferSize;
+	int ToneVolume;
+};
+
+void InitSound_Output(Sound_Output *SoundOutput) {
+	SoundOutput->SamplesPerSecond = 48000;
+	SoundOutput->RunningSampleIndex = 0;
+
+	//Near Note MIDDLE C LOl
+	SoundOutput->ToneHz = 256;
+	SoundOutput->SquareWavePeriod = SoundOutput->SamplesPerSecond / SoundOutput->ToneHz;
+
+	SoundOutput->BytesPerSample = sizeof(int16_t) * 2;
+	SoundOutput->SecondaryBufferSize = SoundOutput->SamplesPerSecond * SoundOutput->BytesPerSample;
+	SoundOutput->ToneVolume = 6000;
+
+}
+
+#define DIRECT_SOUND_CREATE(name) HRESULT WINAPI name(LPGUID lpGuid, LPDIRECTSOUND* ppDS, LPUNKNOWN  pUnkOuter)
+
+typedef DIRECT_SOUND_CREATE(direct_sound_create);
+
+
+
 static void InitDSound(HWND Window, int32_t SamplesPerSecond, int32_t BufferSize) {
 
 	// Load the library
@@ -83,16 +239,16 @@ static void InitDSound(HWND Window, int32_t SamplesPerSecond, int32_t BufferSize
 	HMODULE DSoundLibrary = LoadLibraryA("dsound.dll");
 
 	if (DSoundLibrary) {
-		direct_sound_create *DirectSoundCreate = (direct_sound_create *)
-			GetProcAddress(DSoundLibrary, "DirectSoundCreate");
-		
+
+		direct_sound_create *DirectSoundCreate = (direct_sound_create *)GetProcAddress(DSoundLibrary, "DirectSoundCreate");
+
 		// Get a DirectSound object
 
 		//TODO Check if it works properly on windows 7 or Windows 10
 		LPDIRECTSOUND DirectSound;
-		if (DirectSoundCreate && SUCCEEDED(DirectSoundCreate(0, &DirectSound,0))) {
-			
-			 
+		if (DirectSoundCreate && SUCCEEDED(DirectSoundCreate(0, &DirectSound, 0))) {
+
+
 			//Don't Jumble up the Initializing lines in API
 			WAVEFORMATEX WaveFormat = {};
 			WaveFormat.wFormatTag = WAVE_FORMAT_PCM;
@@ -105,17 +261,17 @@ static void InitDSound(HWND Window, int32_t SamplesPerSecond, int32_t BufferSize
 
 
 			if (SUCCEEDED(DirectSound->SetCooperativeLevel(Window, DSSCL_PRIORITY))) {
-				
+
 				// Create a primary buffer
-				
+
 				DSBUFFERDESC BufferDescription = {};
 				BufferDescription.dwSize = sizeof(BufferDescription);
 				BufferDescription.dwFlags = DSBCAPS_PRIMARYBUFFER;
-								
+
 				LPDIRECTSOUNDBUFFER PrimaryBuffer;
 
 				if (SUCCEEDED(DirectSound->CreateSoundBuffer(&BufferDescription, &PrimaryBuffer, 0))) {
-					
+
 					if (SUCCEEDED(PrimaryBuffer->SetFormat(&WaveFormat))) {
 						OutputDebugStringA("Primary Buffer Created succesfully");
 						//NOTE Format Set!!!
@@ -123,13 +279,11 @@ static void InitDSound(HWND Window, int32_t SamplesPerSecond, int32_t BufferSize
 					else {
 						//NOTE Diagnostic
 					}
-					
+
 				}
 				else {
 					//NOTE Diagnostic
 				}
-
-				
 
 			}
 			else {
@@ -157,127 +311,61 @@ static void InitDSound(HWND Window, int32_t SamplesPerSecond, int32_t BufferSize
 		else {
 			//NOTE Diagnostic
 		}
-		
+
 	}
-
-	
-
-
-	
 
 	// Start playing it
 }
 
-WindowDimension GetWinDimension (HWND Window) {
-	WindowDimension Result;
-	RECT ClientRect;
-	GetClientRect (Window, &ClientRect);
-	Result.Width = ClientRect.right - ClientRect.left;
-	Result.Height = ClientRect.bottom - ClientRect.top;
+static void FillSoundBuffer(Sound_Output *SoundOutput, DWORD ByteToLock, DWORD BytesToWrite) {
+	void *Region1;
+	DWORD Region1Size;
+	void *Region2;
+	DWORD Region2Size;
 
-	return Result;
-}
 
-static void RenderGradient (Offscreen_Buffer *Buffer, int xOffset, int yOffset) {
-	Buffer->Pitch = Buffer->Width * Buffer->BytesPerPixel;
+	//URGENT Run function
+	//SecondaryBuffer->Lock()
+	if (SUCCEEDED(SecondaryBuffer->Lock(ByteToLock, BytesToWrite, &Region1, &Region1Size, &Region2, &Region2Size, 0))) {
+		//TODO assert that RegionSizes are valid
 
-	uint8_t *Row = (uint8_t *)Buffer->Memory;
 
-	for (int y = 0; y < Buffer->Height; ++y) {
+		/*         Region 1                 */
+		int16_t *SampleOut = (int16_t *)Region1;
+		DWORD Region1SampleCount = Region1Size / SoundOutput->BytesPerSample;
 
-		/*
-		incase you want 64bit filling
-		uint64_t *Pixel = (uint64_t *)Row;
-		*/
-		uint32_t *Pixel = (uint32_t *)Row;
+		for (DWORD SampleIndex = 0; SampleIndex < Region1SampleCount; ++SampleIndex) {
 
-		for (int x = 0; x < Buffer->Width; x++) {
-			/*
-				Memory
-				BB GG RR xx
+			float t = 2 * Pi * (float)SoundOutput->RunningSampleIndex / (float)SoundOutput->SquareWavePeriod;
+			float SineValue = sin(t);
+			int16_t SampleValue = (int16_t)(SineValue*SoundOutput->ToneVolume);
+			*SampleOut++ = SampleValue;
+			*SampleOut++ = -SampleValue;
+			++SoundOutput->RunningSampleIndex;
+		}
 
-				When loaded up in memory
+		/*            Region 2              */
+		SampleOut = (int16_t *)Region2;
+		DWORD Region2SampleCount = Region2Size / SoundOutput->BytesPerSample;
 
-				Register
-				xx RR GG BB
-			*/
-			uint8_t Blue = x + xOffset;
-			uint8_t Green = y + yOffset;
-			uint8_t Red = 255;//(x + y + xOffset+yOffset) % 255;
-			
-			/*
+		for (DWORD SampleIndex = 0; SampleIndex < Region2SampleCount; ++SampleIndex) {
 
-			incase you want 64bit filling
-			x++;
+			float t = 2 * Pi * (float)SoundOutput->RunningSampleIndex / (float)SoundOutput->SquareWavePeriod;
+			float SineValue = sin(t);
+			int16_t SampleValue = (int16_t)(SineValue*SoundOutput->ToneVolume);
+			*SampleOut++ = SampleValue;
+			*SampleOut++ = -SampleValue;
+			++SoundOutput->RunningSampleIndex;
+			++SoundOutput->RunningSampleIndex;
+		}
 
-			uint8_t Blue2 = x + *xOffset;
-			uint8_t Green2 = y + *yOffset;
-			uint8_t Red2 = 0;
-
-			
-				BB GG RR xx BB GG RR xx
-				xx RR GG BB xx RR GG BB
-			
-
-			uint64_t temp = ((Red2 << 16) | (Green2 << 8) | Blue2);
-			temp = temp << 32;
-			temp = temp | (Red << 16) | (Green << 8) | (Blue);
-				
-			*Pixel = temp;
-			
-			*/
-
-			*Pixel = (Red << 16) | (Green << 8) | Blue;
-			Pixel++;
-		}	
-		Row += Buffer->Pitch;
+		//SecondaryBuffer->Unlock(Region1, Region1Size, Region2, Region2Size);
 	}
-}
-
-static void WinResizeDIBSection (Offscreen_Buffer *Buffer, int Width, int Height) {
-
-
-	//Free the Memory if already allocated
-	if (Buffer->Memory)
-		VirtualFree (Buffer->Memory, 0, MEM_RELEASE);
-
-
-	Buffer->Height = Height;
-	Buffer->Width = Width;
-
-	Buffer->Info.bmiHeader.biSize = sizeof (Buffer->Info.bmiHeader);
-
-	Buffer->Info.bmiHeader.biWidth = Buffer->Width;
-	//Negative because we want bitmap to be Top down
-	Buffer->Info.bmiHeader.biHeight = -Buffer->Height;
-
-	Buffer->Info.bmiHeader.biPlanes = 1;
-	Buffer->Info.bmiHeader.biBitCount = 32;
-	Buffer->Info.bmiHeader.biCompression = BI_RGB;
-
-	//Number of Pixels we need 
-	//Bytes per pixel Represent
-	// BB GG RR xx 4 bytes
-	int BitMapMemorySize = (Buffer->Width* Buffer->Height)*Buffer->BytesPerPixel;
-
-	Buffer->Memory = VirtualAlloc (0, BitMapMemorySize, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
-
-	//RenderGradient (Buffer, 100, 100);
 
 }
 
-void UpdateWin (Offscreen_Buffer *Buffer, HDC DeviceContext, int WindowWidth, int WindowHeight, int X, int Y/*, int Width, int Height*/) {
+#pragma endregion
 
-	StretchDIBits (DeviceContext,
-
-		//Bitmap gets stretched according to it
-		0, 0, WindowWidth, WindowHeight,
-
-		//BufferWidth remains same
-		0, 0, Buffer->Width, Buffer->Height,
-
-		Buffer->Memory, &Buffer->Info, DIB_RGB_COLORS, SRCCOPY);
-}
 
 LRESULT CALLBACK MainProc (HWND Window, UINT Message, WPARAM wParam, LPARAM lParam) {
 	LRESULT Result = 0;
@@ -294,7 +382,7 @@ LRESULT CALLBACK MainProc (HWND Window, UINT Message, WPARAM wParam, LPARAM lPar
 		int Height = Paint.rcPaint.bottom - Y;
 
 
-		WindowDimension Dimension = GetWinDimension (Window);
+		Window_Dimension Dimension = GetWinDimension (Window);
 		UpdateWin (&BackBuffer, DeviceContext, Dimension.Width, Dimension.Height, X, Y/*, Width, Height*/);
 
 		EndPaint (Window, &Paint);
@@ -407,24 +495,14 @@ int CALLBACK WinMain (HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CmdLine,
 
 
 		/*         For Sound              */
-		int SamplesPerSecond = 48000;
-		uint32_t RunningSampleIndex = 0;
-
-		//Near Note MIDDLE C LOl
-		int ToneHz = 256;
-		int SquareWaveCounter = 0;
-		int SquareWavePeriod = SamplesPerSecond / ToneHz;
-		int HalfSquareWavePeriod = SquareWavePeriod / 2;
-		int BytesPerSample = sizeof(int16_t) * 2;
-		int SecondaryBufferSize = SamplesPerSecond * BytesPerSample;
-		int ToneVolume = 250;
-
+		Sound_Output SoundOutput;
+		InitSound_Output(&SoundOutput);
 		
 		//Initialize our Secondary Buffer 
-		InitDSound(Window, SamplesPerSecond, SamplesPerSecond * BytesPerSample);
-
-		//Play our Buffer
+		InitDSound(Window, SoundOutput.SamplesPerSecond, SoundOutput.SecondaryBufferSize);
+		FillSoundBuffer(&SoundOutput, 0, SoundOutput.SecondaryBufferSize);
 		SecondaryBuffer->Play(0, 0, DSBPLAY_LOOPING);
+		//bool SoundIsPlaying = false;
 		/*         For Sound              */
 
 
@@ -480,71 +558,40 @@ int CALLBACK WinMain (HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CmdLine,
 			RenderGradient(&BackBuffer, xOffset, yOffset);
 
 
-
 			/*         For Sound           */
 			DWORD PlayCursor;
 			DWORD WriteCursor;
-			if (SUCCEEDED(SecondaryBuffer->GetCurrentPosition(&PlayCursor, &WriteCursor))) {
+			if ( SUCCEEDED(SecondaryBuffer->GetCurrentPosition(&PlayCursor, &WriteCursor))) {
 				
-				DWORD ByteToLock = RunningSampleIndex * BytesPerSample % SecondaryBufferSize;
 				DWORD BytesToWrite;
+				DWORD BytesToLock = (SoundOutput.RunningSampleIndex * SoundOutput.BytesPerSample) % SoundOutput.SecondaryBufferSize;
 				
+				//NOTE More accurate check for ByteToLock == PlayCursor
+				if (BytesToLock == PlayCursor) {
+					//if (!SoundIsPlaying)
+						//BytesToWrite = SoundOutput.SecondaryBufferSize;
+					//else
+						BytesToWrite = 0;
+				}
+
 				//The Case when we have to do two chunks i.e. chunk after BytesToLock and 
 				//the chunk before PlayCursor
-				if (ByteToLock > PlayCursor) {
-					BytesToWrite = (SecondaryBufferSize - ByteToLock);
+				else if (BytesToLock > PlayCursor) {
+					BytesToWrite = (SoundOutput.SecondaryBufferSize - BytesToLock);
 					BytesToWrite += PlayCursor;
 				}
 				//When only one chunk is to be filled
 				else {
-					BytesToWrite = PlayCursor - ByteToLock;
+					BytesToWrite = PlayCursor - BytesToLock;
 				}
-
-				//INIT
-				DWORD WritePointer;
-
-				void *Region1;
-				DWORD Region1Size;
-				void *Region2;
-				DWORD Region2Size;
+				FillSoundBuffer(&SoundOutput, BytesToLock, BytesToWrite);
 				
-
-				//URGENT Run function
-				//SecondaryBuffer->Lock()
-				if (SUCCEEDED(SecondaryBuffer->Lock(ByteToLock, BytesToWrite, &Region1, &Region1Size, &Region2, &Region2Size, 0))) {
-					//TODO assert that RegionSizes are valid
-
-
-					/*         Region 1                 */
-					int16_t *SampleOut = (int16_t *)Region1;
-					DWORD Region1SampleCount = Region1Size / BytesPerSample;
-
-					for (DWORD SampleIndex = 0; SampleIndex < Region1SampleCount; ++SampleIndex) {
-						
-						int16_t SampleValue = ((RunningSampleIndex++ / HalfSquareWavePeriod) % 2) ? ToneVolume : -ToneVolume;
-						*SampleOut++ = SampleValue;
-						*SampleOut++ = SampleValue;
-					}
-					
-					/*            Region 2              */
-					SampleOut = (int16_t *)Region2;
-					DWORD Region2SampleCount = Region2Size / BytesPerSample;
-
-					for (DWORD SampleIndex = 0; SampleIndex < Region2SampleCount; ++SampleIndex) {
-						
-						int16_t SampleValue = ((RunningSampleIndex++ / HalfSquareWavePeriod) % 2) ? ToneVolume : -ToneVolume;
-						*SampleOut++ = SampleValue;
-						*SampleOut++ = SampleValue;
-					}
-
-					//SecondaryBuffer->Unlock(Region1, Region1Size, Region2, Region2Size);
-				}
-
 			}
+
 			/*         For Sound           */
 
 
-			WindowDimension Dimension = GetWinDimension(Window);
+			Window_Dimension Dimension = GetWinDimension(Window);
 
 			UpdateWin(&BackBuffer, DeviceContext, Dimension.Width, Dimension.Height, 0, 0/*,Dimension.Width,Dimension.Height*/);
 			//ReleaseDC (Window, DeviceContext);
